15d01744069c967d6c7e31aa644b1eb815fee978
Michael Rocha Nunes <michaelrn2288@gmail.com>
2022-12-06 23:48:48 -0300

    set Sparkle initial position

--- src/Components/Sparkle.jsx
+++ src/Components/Sparkle.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled, { keyframes } from 'styled-components'
 
 const sparkleAnimation = keyframes`
@@ -20,8 +20,8 @@ border-radius: 50%;
 background-color: rgb(255, 255, 0);
 box-shadow: inset 0 0 12px rgb(255,255,255),
             0 0 12px rgb(255,255,0);
-left: ${props => props.mousePositionX}px;
-top: ${props => props.mousePositionY}px;
+left: ${props => props.initialPosition.x}px;
+top: ${props => props.initialPosition}px;
 animation-name: ${sparkleAnimation};
 animation-duration: 2.5s;
 animation-iteration-count: infinite;
@@ -29,12 +29,23 @@ animation-iteration-count: infinite;
 
 export default function Sparkle(props) {
 
+    const [initialPosition, setInitialPosition] = useState({
+        x: '',
+        y: ''
+    })
+
+    useEffect(() => {
+        setInitialPosition({
+            x: props.mousePositionX,
+            y: props.mousePositionY
+        })
+    }, [])
+
     return (
         <StyledSparkle
             mousePositionX={props.mousePositionX}
             mousePositionY={props.mousePositionY}
-        >
-
-        </StyledSparkle>
+            initialPosition={initialPosition}
+        />
     )
 }
